e54c70e28cd44eab486947be8749d29d9a5e5e47
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-17 21:39:20 -0700

    sumEvenTo1000()

--- foundation.js
+++ foundation.js
@@ -26,3 +26,14 @@ var count255 = function(){
     }
     return out;
 }
+
+//Get even 1000 - Write a function that would get the sum of all the even numbers from 1 to 1000.  You may use a modulus operator for this exercise.
+var sumEvenTo1000 = function(){
+    var sum = 0;
+    for(var i = 0; i <=1000;i++){
+        if(i%2==0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
