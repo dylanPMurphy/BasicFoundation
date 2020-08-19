e2ec6bc05d6e835e2b84943eee38fb04dae3fa39
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-18 22:59:42 -0700

    swapFirstLast()

--- basicFoundation.js
+++ basicFoundation.js
@@ -121,7 +121,13 @@ var findMaxMinAvg = function (inputArray){
 }
 
 //Swap Values - Write a function that will swap the first and last values of any given array. The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
+var swapFirstLast = function(inputArray){
+    var temp = inputArray[0];
+    inputArray[0] = inputArray[inputArray.length-1];
+    inputArray[inputArray.length-1] = temp;
+    return inputArray;
 
+}
 
 //Number to String - Write a function that takes an array of numbers and replaces any negative values within the array with the string 'Dojo'. For example if array = [-1,-3,2], your function will return ['Dojo','Dojo',2].
 
@@ -164,3 +170,5 @@ testArray = [23,52,4,3,13,5, -54, 210, -9, 0, -123432, -0];
 
 console.log(findMaxMinAvg(testArray));
 testArray = [23,52,4,3,13,5, -54, 210, -9, 0, -123432, -0];
+
+console.log(swapFirstLast(testArray));
